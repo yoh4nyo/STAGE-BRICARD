8e434de77d3fdbfda9d3d374d408f3d849364af1
Som <ysom@allegion.com>
2025-04-01 14:06:01 +0200

    angular installation

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,6 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
 
-createApp(App).mount('#app')
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
